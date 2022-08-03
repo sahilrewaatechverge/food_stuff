5d8a77f6083111a29f33aba2d3aa961ccb032dcd
sahilrewaatechverge <sahil.rewaatechverge@gmail.com>
2022-08-03 13:09:20 +0530

    add constant value and fix minor overflow

--- lib/components/widgets.dart
+++ lib/components/widgets.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Widgets{
-  static Widget coustomText({required String data, required TextStyle style}){
+  static Widget customText({required String data, required TextStyle style}){
     return Text(data,style: style,);
   }
 }
